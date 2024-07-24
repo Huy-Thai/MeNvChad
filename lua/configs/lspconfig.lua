0441536016e15564025762c86a437001e814adca
Huy-Thai <huythai.dev@outlook.com>
2024-07-24 09:32:15 +0700

    config for rust javascript typescript tailwindcss html css prettier

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,10 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local util = require "lspconfig/util"
+local servers = { "tsserver", "tailwindcss", "eslint", "html", "cssls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +15,16 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
+-- rust
+lspconfig.rust_analyzer.setup({
   on_attach = on_attach,
-  on_init = on_init,
   capabilities = capabilities,
-}
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = { allFeatures = true },
+    },
+  },
+})
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,33 +6,64 @@ return {
       require "configs.conform"
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "rust-analyzer",
+        "eslint-lsp",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      },
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+  },
+  {
+   	"nvim-treesitter/nvim-treesitter",
+    opts = {
+    ensure_installed = {
+   		"vim",
+      "lua",
+      "vimdoc",
+      "html",
+      "css",
+      "rust",
+      "javascript",
+      "typescript",
+      "tsx",
+   	}},
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  }
 }
