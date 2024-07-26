063035270fc9a460e9d26f109e50176b9452ecd9
Huy-Thai <huythai.dev@outlook.com>
2024-07-26 15:27:32 +0700

    update guifont

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+vim.opt.guifont = { "JetBrainsMono Nerd Font", "h12" }
 
 vim.api.nvim_create_autocmd("QuitPre", {
   callback = function()
